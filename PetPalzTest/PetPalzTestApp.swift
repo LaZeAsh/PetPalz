dbbee4a42b1695d649d1f5540ef7628486271fcb
LaZeAsh <ayushrgarg@gmail.com>
2022-12-28 17:50:14 -0500

    Initial Commit

---
+++ PetPalzTest/PetPalzTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetPalzTestApp.swift
+//  PetPalzTest
+//
+//  Created by Ash on 12/28/22.
+//
+
+import SwiftUI
+
+@main
+struct PetPalzTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
