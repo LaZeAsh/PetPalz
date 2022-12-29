8b9b8f454cb983d00a5d7708afd870c821653403
LaZeAsh <ayushrgarg@gmail.com>
2022-12-28 20:45:01 -0500

    Make base for app

--- PetPalzTest/ContentView.swift
+++ PetPalzTest/ContentView.swift
@@ -9,13 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+//        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundColor(.accentColor)
+//            Text("Hello, world!")
+//        }
+//        .padding()
+        ZStack (alignment: .top){
+            Color.green.ignoresSafeArea(.all)
+            Text("PetPalz")
+                .multilineTextAlignment(.center)
+                .scaleEffect(2)
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 100)
+                .foregroundColor(.white)
         }
-        .padding()
     }
 }
 
